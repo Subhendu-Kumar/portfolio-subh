ba4ef42cdb86b8943c16cd3f20436c5f564bbdcb
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2024-07-07 17:57:43 +0530

    fixes

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -5,7 +5,7 @@ import { BackgroundGradientAnimation } from "./background-gradient-animation";
 import { GlobeDemo } from "./GridGlobe";
 import Lottie from "react-lottie";
 import { useState } from "react";
-import animationData from "@/app/data/confetti.json";
+import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
 
