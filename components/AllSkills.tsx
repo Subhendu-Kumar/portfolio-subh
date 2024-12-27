22b2bc2d2f443a9bf9a1f432ab061e1b2fa7629c
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2024-12-27 20:33:24 +0530

    fix

--- components/AllSkills.tsx
+++ components/AllSkills.tsx
@@ -5,13 +5,16 @@ const AllSkills = () => {
     <div className="py-20 w-full h-auto" id="projects">
       <h1 className="heading text-purple">My Skills</h1>
       <div className="w-full h-auto flex flex-wrap items-center justify-center gap-6 mt-10">
-        {skills.map((src, idx) => {
+        {skills.map((data, idx) => {
           return (
             <div
               key={idx}
-              className="w-32 h-32 bg-black border rounded-full justify-center flex items-center"
+              className="w-32 h-32 cursor-pointer hover:scale-110 transition-all duration-300 ease-in-out bg-black border border-white/[0.5] rounded-full justify-center flex items-center relative group"
             >
-              <img src={src} alt="logo" className="w-20 h-20" />
+              <img src={data.image} alt={data.name} className="w-20 h-20" />
+              <span className="absolute bottom-3 left-1/2 transform -translate-x-1/2 translate-y-6 bg-gray-500 text-white text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                {data.name}
+              </span>
             </div>
           );
         })}
